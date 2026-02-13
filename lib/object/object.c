d0329241b24b28375df9555b45042f185e2b65f1
seb hall <sebhalldesigns@gmail.com>
2026-02-13 15:18:49 +0000

    initial object structure

--- lib/object/object.c
+++ lib/object/object.c
@@ -27,6 +27,55 @@
 ** MARK: TYPEDEFS
 ***************************************************************/
 
+struct object_t;
+
+typedef struct
+{
+    /* name of the method */
+    char* name;
+
+    /* function pointer, self is an object_t */
+    void (*function)(struct object_t *self, void *args, void *result);
+} method_t;
+
+typedef struct 
+{
+    /* name of the property */
+    char* name;
+
+    /* data layout - always GLOBAL offset in instance data */
+    size_t offset;
+    size_t size;
+} property_t;
+ 
+typedef struct type_t
+{
+    /* name of the type */
+    char* name; 
+
+    /* parent for inheritance */
+    struct type_t *parent; /* can be NULL */
+    
+    size_t local_data_size; /* data size just for this type */
+    size_t total_data_size; /* combined data size of this and all parent types */
+
+    /* properties should always be defined with global layout
+    ** i.e offset by total_data_size - local_data_size 
+    ** this is so that each instance's data can be contiguous
+    */
+    property_t *properties;
+    size_t property_count;
+
+    method_t *methods;
+    size_t method_count;
+} type_t;
+
+typedef struct
+{
+    type_t *type; /* pointer to the type of this object */
+    void *data; /* pointer to the raw data */
+} object_t;
+
 /***************************************************************
 ** MARK: STATIC VARIABLES
 ***************************************************************/
--- lib/object/object.h
+++ lib/object/object.h
@@ -32,12 +32,29 @@ extern "C" {
 ** MARK: TYPEDEFS
 ***************************************************************/
 
-typedef uintptr_t object_t;
+
+typedef uintptr_t type_handle_t;
+typedef uintptr_t property_handle_t;
+typedef uintptr_t method_handle_t;
+
+typedef uintptr_t object_handle_t;
 
 /***************************************************************
 ** MARK: FUNCTION DEFS
 ***************************************************************/
 
+type_handle_t object_get_type(const char* name);
+property_handle_t object_get_property(const char* name, type_handle_t type_handle);
+method_handle_t object_get_method(const char* name, type_handle_t type_handle);
+
+object_handle_t object_create(type_handle_t type_handle);
+void object_set_property(object_handle_t object_handle, property_handle_t property_handle, const void* value);
+
+void object_call_method(object_handle_t object_handle, method_handle_t method_handle, const void* args, void* result);
+
+type_handle_t object_register_type(const char* name, type_handle_t parent_type_handle, size_t local_data_size);
+void object_register_property(type_handle_t type_handle, const char* name, size_t offset, size_t size);
+void object_register_method(type_handle_t type_handle, const char* name, void (*function)(object_handle_t, void*, void*));
 
 #ifdef __cplusplus
 }
